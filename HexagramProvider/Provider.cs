fa6d75e11ece432a4c52409ab680971bda5458e8
yueyinqiu <lujiaqi2003@126.com>
2022-09-23 20:31:37 +0800

    rup

---
+++ HexagramProvider/Provider.cs
@@ -0,0 +1,20 @@
+﻿using YiJingFramework.Core;
+
+namespace HexagramProvider
+{
+    public static class Provider
+    {
+        private static IEnumerable<YinYang> RandomYinYangs(int seed)
+        {
+            Random random = new Random(seed);
+            for (; ; )
+                yield return (YinYang)random.Next(0, 2);
+        }
+
+        public static Painting GetHexagram(DateOnly date)
+        {
+            var seed = date.DayNumber;
+            return new Painting(RandomYinYangs(seed).Take(6));
+        }
+    }
+}
